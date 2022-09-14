e623e33631a01f5005d4d164b98937bf513d120b
Syed Masaab Ahmed <50544638+syedMasab@users.noreply.github.com>
2022-09-14 23:42:31 +0500

    Add files via upload

---
+++ python crash course Ch#7.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+car = input("What kind of car would you like? ")
+
+print("Let me see if I can find you a " + car.title() + ".")
+
+
+# In[3]:
+
+
+party_size = input("How many people are in your dinner party tonight? ")
+party_size = int(party_size)
+
+if party_size > 8:
+    print("I'm sorry, you'll have to wait for a table.")
+else:
+    print("Your table is ready.")
+
+
+# In[4]:
+
+
+number = input("Give me a number, please: ")
+number = int(number)
+
+if number % 10 == 0:
+    print(str(number) + " is a multiple of 10.")
+else:
+    print(str(number) + " is not a multiple of 10.")
+
+
+# In[5]:
+
+
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    topping = input(prompt)
+    if topping != 'quit':
+        print("  I'll add " + topping + " to your pizza.")
+    else:
+        break
+
+
+# In[6]:
+
+
+prompt = "How old are you?"
+prompt += "\nEnter 'quit' when you are finished. "
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    age = int(age)
+
+    if age < 3:
+        print("  You get in free!")
+    elif age < 13:
+        print("  Your ticket is $10.")
+    else:
+        print("  Your ticket is $15.")
+
+
+# In[7]:
+
+
+sandwich_orders = ['veggie', 'grilled cheese', 'turkey', 'roast beef']
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print("I'm working on your " + current_sandwich + " sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich + " sandwich.")
+
+
+# In[8]:
+
+
+name_prompt = "\nWhat's your name? "
+place_prompt = "If you could visit one place in the world, where would it be? "
+continue_prompt = "\nWould you like to let someone else respond? (yes/no) "
+
+# Responses will be stored in the form {name: place}.
+responses = {}
+
+while True:
+    # Ask the user where they'd like to go.
+    name = input(name_prompt)
+    place = input(place_prompt)
+
+    # Store the response.
+    responses[name] = place
+
+    # Ask if there's anyone else responding.
+    repeat = input(continue_prompt)
+    if repeat != 'yes':
+        break
+
+# Show results of the survey.
+print("\n--- Results ---")
+for name, place in responses.items():
+    print(name.title() + " would like to visit " + place.title() + ".")
+
+
+# In[ ]:
+
+
+
+
